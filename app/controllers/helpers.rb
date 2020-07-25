c9590cb9e02c00e881e68c864c787a345435a26c
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-24 22:16:46 -0700

    Done.

--- app/controllers/helpers.rb
+++ app/controllers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-    def self.logged_in?
+    def self.logged_in?(session)
         !!session[:user_id]
     end
 
-    def self.current_user
+    def self.current_user(session)
         User.find(session[:user_id])
     end
 end
